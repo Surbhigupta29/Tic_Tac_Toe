0e96ab8760efb59af817ad10fe270079de15ded5
Surbhigupta29 <60618886+Surbhigupta29@users.noreply.github.com>
2020-02-06 21:17:19 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Game from "./game";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Game />, rootElement);
